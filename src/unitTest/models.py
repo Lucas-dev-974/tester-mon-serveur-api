cfdf59af99d503d92cab73f6e4d354dbf9a83618
lucas <lucas.lvn97439@gmail.com>
2023-06-19 15:24:27 +0400

    add game model

--- src/unitTest/models.py
+++ src/unitTest/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=30)
+    editor = models.CharField(max_length=30)
+    nb_players = models.IntegerField(max_length=30)
